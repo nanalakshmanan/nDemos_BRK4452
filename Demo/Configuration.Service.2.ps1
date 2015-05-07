6250777f182f967ea010b2d2850761ad38650e6e
Nana Lakshmanan <nanalakshmanan@gmail.com>
2015-05-06 22:26:50 -0500

    Services demo

---
+++ Demo/Configuration.Service.2.ps1
@@ -0,0 +1,60 @@
+﻿Configuration MyServices
+{    
+    param
+    (
+        # Target nodes to apply the configuration
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [String[]]$NodeName
+    )
+
+    # Import the module that defines custom resources
+    Import-DscResource -Module xNetworking
+
+    Node $NodeName
+    {
+        # Ensure remote desktop and error reporting are not running
+        foreach($Service in @('TermService', 'Wersvc'))
+        {
+            Service "$Service"
+            {
+                Name        = $Service
+                State       = 'Stopped'
+                StartupType = 'Manual'
+            }
+        }
+
+        # Install the IIS role
+        WindowsFeature IIS
+        {
+            Ensure          = 'Present'
+            Name            = 'Web-Server'
+        }
+
+        # Ensure w3svc is running
+        Service w3svc
+        {
+            Name        = 'w3svc'
+            State       = 'Running'
+            StartupType = 'Automatic'
+            DependsOn   = '[WindowsFeature]IIS'
+        }
+               
+        # Ensure port 80 is open
+        xFirewall Port80
+        {
+            Access       = 'Allow'
+            Name         = 'Port80'
+            Description  = 'Allow port 80 open for web connections'
+            Direction    = 'Inbound'
+            DisplayGroup = 'MSIgnite'
+            DisplayName  = 'Firewall rule for MSIgnite'
+            Ensure       = 'Present'
+            LocalPort    = '80'
+            RemotePort   = '80'
+            Profile      = 'Any'
+            Protocol     = 'TCP'            
+            State        = 'Enabled'
+       }
+    }
+}
---
+++ Demo/MyServices.ps1
@@ -0,0 +1,22 @@
+﻿$ScriptPath = Split-Path $MyInvocation.MyCommand.Path
+. "$ScriptPath\0-CommonInit.ps1"
+
+# discover if there are required resources
+Find-DscResource -Name xFirewall
+
+# get the module
+Install-Module xNetworking -Verbose -Force
+
+# now start writing a configuration
+psedit "$ScriptPath\Configuration.Service.1.ps1"
+
+# configurations are like functions - so they can have parameters
+# and help
+psedit "$ScriptPath\Configuration.Service.2.ps1"
+
+# Now you can invoke a configuration
+psedit "$ScriptPath\Invoke-MyServices.ps1"
+
+# This configuration can now be packaged in a module for sharing
+psedit "$ScriptPath\..\MyServices\MyServices.psm1"
+
